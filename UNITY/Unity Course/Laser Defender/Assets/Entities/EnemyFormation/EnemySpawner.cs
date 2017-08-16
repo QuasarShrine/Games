66f7ffb6708114fa89e44a506dea99d8e5c33e59
WegPast <wegpast@gmail.com>
2017-08-16 13:26:48 +0200

    Adding Position to make enemy appear on the right place.

---
+++ UNITY/Unity Course/Laser Defender/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+
+    public float width = 10f;
+    public float height = 5f;
+
+	// Use this for initialization
+	void Start () {
+        foreach ( Transform child in transform) {
+            GameObject enemy = Instantiate(enemyPrefab, child.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+	}
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
