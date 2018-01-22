d7cb2d9b21e1206b041cf7b7cfc884e12363dda6
WegPast <wegpast@gmail.com>
2018-01-22 13:38:28 +0100

    Moving ball and tracking camera part 1

---
+++ UNITY/Unity Course/Bowlmaster/Assets/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody body;
+    public float launchSpeed;
+
+    private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+        Launch();
+
+    }
+
+    private void Launch() {
+        body.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ UNITY/Unity Course/Bowlmaster/Assets/CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Ball ball;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = new Vector3(0, 30, -100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = ball.transform.position + offset;
+	}
+}
