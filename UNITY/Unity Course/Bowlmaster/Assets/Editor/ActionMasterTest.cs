16de4ba3a5a48ceaa81b3aa9d6575b1cf3b5692a
WegPast <wegpast@gmail.com>
2018-02-26 13:40:13 +0100

    TDD TDD TDD....

--- UNITY/Unity Course/Bowlmaster/Assets/Editor/ActionMasterTest.cs
+++ UNITY/Unity Course/Bowlmaster/Assets/Editor/ActionMasterTest.cs
@@ -40,11 +40,41 @@ public class ActionMasterTest
 
     [Test]
     public void T04CheckResetAtStrikeInLastFrame() {
-        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, };
+        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
         foreach (int roll in rolls) {
             actionMaster.Bowl(roll);
         }
         Assert.AreEqual(reset, actionMaster.Bowl(10));
+    }
+
+
+    [Test]
+    public void T05CheckResetAtStrikeInLastFrame() {
+        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        foreach (int roll in rolls) {
+            actionMaster.Bowl(roll);
+        }
+        actionMaster.Bowl(1);
+        Assert.AreEqual(reset, actionMaster.Bowl(9));
+    }
+
+    [Test]
+    public void T06YouTubeRollsEndInEndGame() {
+        int[] rolls = { 8, 2, 7, 3, 3, 4, 10, 2, 8, 10, 10, 8, 0, 10, 8, 2};
+        foreach (int roll in rolls) {
+            actionMaster.Bowl(roll);
+        }
+        Assert.AreEqual(endGame, actionMaster.Bowl(9));
+    }
+
+
+    [Test]
+    public void T07GameEndsAtBowl20() {
+        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        foreach (int roll in rolls) {
+            actionMaster.Bowl(roll);
+        }
+        Assert.AreEqual(endGame, actionMaster.Bowl(1));
     }
 
 }
--- UNITY/Unity Course/Bowlmaster/Assets/Scripts/ActionMaster.cs
+++ UNITY/Unity Course/Bowlmaster/Assets/Scripts/ActionMaster.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ActionMaster
 {
-    // private int[] bowls = new int[21];
+    private int[] bowls = new int[21];
     private int bowl = 1;
 
     public enum Action{ Tidy, Reset, EndTurn, EndGame };
@@ -13,7 +13,19 @@ public class ActionMaster
         // garde code
         if (pins > 10 || pins < 0) { throw new UnityException("Invalid pins number!"); }
 
+        bowls[bowl - 1] = pins;
+
+        if(bowl == 21) {
+            return Action.EndGame;
+        }
+
         // Other behaviour here, e.g last frame
+        if(bowl >=19 && Bowl21Awareded()) {
+            bowl += 1;
+            return Action.Reset;
+        }else if(bowl == 20 && !Bowl21Awareded()) {
+            return Action.EndGame;
+        }
 
         if (pins == 10) {
             bowl += 2;
@@ -29,5 +41,9 @@ public class ActionMaster
         }
 
         throw new UnityException("Not sure what action to return!");
+    }
+
+    private bool Bowl21Awareded() {
+        return (bowls[19 - 1] + bowls[20 - 1] >= 10);   
     }
 }
