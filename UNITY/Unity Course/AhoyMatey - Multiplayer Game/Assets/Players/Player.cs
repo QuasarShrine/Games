3b621921f185e8d5b6743949038be644cefad361
WegPast <wegpast@gmail.com>
2018-03-22 13:42:40 +0100

    adding Standard Assets and network movement

---
+++ UNITY/Unity Course/AhoyMatey - Multiplayer Game/Assets/Players/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class Player : NetworkBehaviour {
+
+    private Vector3 inputValues;
+
+	// Use this for initialization
+	void Start () {
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer) {
+            return;
+        }
+            inputValues.x = CrossPlatformInputManager.GetAxis("Horizontal");
+            inputValues.y = 0f;
+            inputValues.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+            transform.Translate(inputValues);
+	}
+}
