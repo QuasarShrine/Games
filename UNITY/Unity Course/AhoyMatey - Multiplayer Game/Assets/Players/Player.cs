aaa8dbdb6f97ca3917d6ad2366dc34fb9dcbadd8
WegPast <wegpast@gmail.com>
2018-03-23 13:56:06 +0100

    End of Ahoy matey section. Know go make a multiplayer game from that

--- UNITY/Unity Course/AhoyMatey - Multiplayer Game/Assets/Players/Player.cs
+++ UNITY/Unity Course/AhoyMatey - Multiplayer Game/Assets/Players/Player.cs
@@ -4,25 +4,29 @@ using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine.Networking;
 using UnityEngine;
 
-public class Player : NetworkBehaviour {
+public class Player : NetworkBehaviour
+{
 
     private Vector3 inputValues;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
 
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         if (!isLocalPlayer) {
             return;
         }
-            inputValues.x = CrossPlatformInputManager.GetAxis("Horizontal");
-            inputValues.y = 0f;
-            inputValues.z = CrossPlatformInputManager.GetAxis("Vertical");
+        inputValues.x = CrossPlatformInputManager.GetAxis("Horizontal");
+        inputValues.y = 0f;
+        inputValues.z = CrossPlatformInputManager.GetAxis("Vertical");
 
-            transform.Translate(inputValues);
-	}
+        transform.Translate(inputValues);
+    }
+    public override void OnStartLocalPlayer() {
+        GetComponentInChildren<Camera>().enabled = true;
+    }
 }
