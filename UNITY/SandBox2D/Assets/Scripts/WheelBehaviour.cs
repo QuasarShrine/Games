eef46e1f341f1d845361a6af05d613e4f024c4f5
WegPast <wegpast@gmail.com>
2018-09-18 23:41:35 +0200

    - Added Slope - Added wheel - Added camera target controler

---
+++ UNITY/SandBox2D/Assets/Scripts/WheelBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelBehaviour : MonoBehaviour {
+
+  [Range(1f,10f)]
+  public float acceleration;
+
+  private Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+    rigidbody2D = GetComponent<Rigidbody2D>();
+
+  }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+    if (Input.GetKey(KeyCode.LeftArrow)) {
+      rigidbody2D.AddForce(Vector2.left * acceleration);
+    }
+
+    if (Input.GetKey(KeyCode.RightArrow)) {
+      rigidbody2D.AddForce(Vector2.right * acceleration);
+    }
+  }
+
+
+}
